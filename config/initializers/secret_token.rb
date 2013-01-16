7f18f64410e7fb6e3d10f069bee22070f2e871d6
Wahyu Sumartha <wahyusumartha@gmail.com>
2013-01-16 21:08:52 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereToEat::Application.config.secret_token = 'c4fe23bdb6dbcdb674206db05224112737b84578f86efacdc44443e2a1a719d96bef553ded421b28588cc3d77f2e6a6ddc4a0d47f48434bed3a4089ad9e7f55f'
